e392f96d70bfcf70ea836593424b49afe2960a21
Amatul noor <amatulndmk@gmail.com>
2022-01-19 11:29:46 +0700

    home_page

--- application/views/user/include/footer.php
+++ application/views/user/include/footer.php
@@ -1,30 +1,5 @@
 <!-- Newsletter -->
 
-<div class="newsletter">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6">
-				<div
-					class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-					<h4>Newsletter</h4>
-					<p>Subscribe to our newsletter and get 20% off your first purchase</p>
-				</div>
-			</div>
-			<div class="col-lg-6">
-				<form action="post">
-					<div
-						class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
-						<input id="newsletter_email" type="email" placeholder="Your email" required="required"
-							data-error="Valid email is required.">
-						<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300"
-							value="Submit">subscribe</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
-
 <!-- Footer -->
 
 <footer class="footer">
@@ -35,8 +10,7 @@
 					class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
 					<ul class="footer_nav">
 						<li><a href="#">Blog</a></li>
-						<li><a href="#">FAQs</a></li>
-						<li><a href="contact.html">Contact us</a></li>
+						<li><a href="contact.html">Hubungi kami</a></li>
 					</ul>
 				</div>
 			</div>
@@ -47,8 +21,6 @@
 						<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
 						<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 						<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
 					</ul>
 				</div>
 			</div>
@@ -56,9 +28,7 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="footer_nav_container">
-					<div class="cr">©2018 All Rights Reserverd. Made with <i class="fa fa-heart-o"
-							aria-hidden="true"></i> by <a href="#">Colorlib</a> &amp; distributed by <a
-							href="https://themewagon.com">ThemeWagon</a></div>
+					<div class="cr">©2021 All Rights Reserved.</div>
 				</div>
 			</div>
 		</div>
