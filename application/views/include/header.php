47b25fac7911fa0a79527dbc7a710a12089f6515
Amatul noor <amatulndmk@gmail.com>
2022-01-19 10:59:18 +0700

    yeyyyy

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-	<title>Dashboard &mdash; Adminnnnn</title>
+	<title>Dashboard &mdash; Admin</title>
 	<!-- Logo  -->
 	<link rel="shortcut icon" href="">
 
