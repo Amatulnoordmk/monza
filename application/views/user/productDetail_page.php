7822966a89facad99f3ca03fabd7588209b9ade9
karvin2612 <karvin2612@gmail.com>
2022-01-20 08:48:50 +0700

    detailproduk - gambar dll

--- application/views/user/productDetail_page.php
+++ application/views/user/productDetail_page.php
@@ -19,20 +19,20 @@
 		<div class="col-lg-7">
 			<div class="single_product_pics">
 				<div class="row">
-					<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
+					<!-- <div class="col-lg-3 thumbnails_col order-lg-1 order-2">
 						<div class="single_product_thumbnails">
 							<ul>
-								<li><img src="images/single_1_thumb.jpg" alt="" data-image="images/single_1.jpg"></li>
+								<li><img src="<?=base_url();?>assets/user/images/product_1.png" alt="" data-image="<?=base_url();?>assets/user/images/single_1.jpg"></li>
 								<li class="active"><img src="images/single_2_thumb.jpg" alt=""
 										data-image="images/single_2.jpg"></li>
 								<li><img src="images/single_3_thumb.jpg" alt="" data-image="images/single_3.jpg"></li>
 							</ul>
 						</div>
-					</div>
+					</div> -->
 					<div class="col-lg-9 image_col order-lg-2 order-1">
 						<div class="single_product_image">
 							<div class="single_product_image_background"
-								style="background-image:url(images/single_2.jpg)"></div>
+								style="background-image:url(<?=base_url();?>assets/user/images/product_1.png)"></div>
 						</div>
 					</div>
 				</div>
@@ -41,13 +41,13 @@
 		<div class="col-lg-5">
 			<div class="product_details">
 				<div class="product_details_title">
-					<h2>Nama Barang Muncul Disini</h2>
+					<h2>Jaket Warna Coklat</h2>
 					<div class="product_price">Rp 50.000</div>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, excepturi minima nostrum vero earum voluptatibus atque exercitationem sequi laudantium inventore eveniet similique consectetur velit fuga adipisci nesciunt veniam minus reprehenderit.</p>
-					<p>Lokasi : <span class="product_price">Alamat disini</span></p>
+					<p>Jaket warna coklat. Ukuran L. Baru dipakai 3-4 kali. Ingin dibagikan karena sudah jarang dipakai. Jika tertarik harap hubungi kontak saya :)</p>
+					<p>Lokasi : <span class="product_price">Jl. Marelan Pasar 4</span></p>
 					<br>
-					<h3>Diposting Oleh : <span>Nama Disini</span></h3>
-					<p>No HP : <span>0812-xxxx-xxxx</span></p>
+					<h4>Diposting Oleh : <span>Mei Mei</span></h4>
+					<p>No HP : <span>0812-2233-4455</span></p>
 				</div>
 				<div class="free_delivery d-flex flex-row align-items-center justify-content-center">
 					<span class="ti-mobile"></span><span>Hubungi via WhatsApp</span>
@@ -79,8 +79,9 @@
 					<div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
 					<div class="product_favorite d-flex flex-column align-items-center justify-content-center"></div>
 				</div> -->
+				<br>
 				<div class="alert alert-danger d-flex align-items-center" role="alert">
-  					<svg svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  					<!-- <svg svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg> -->
   					<div>
    						<b>Pembayaran hanya via COD.</b> Mohon berhati-hati dalam bertransaksi. Monza hanyalah sarana publikasi. Segala kegiatan transaksi diluar tanggung jawab Monza.
   					</div>
