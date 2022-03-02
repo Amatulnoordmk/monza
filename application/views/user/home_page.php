d116b6d83493213aa9fc94e5b207cab98cb0aae3
Amatul noor <amatulndmk@gmail.com>
2022-03-02 11:02:41 +0700

    slider gambar

--- application/views/user/home_page.php
+++ application/views/user/home_page.php
@@ -144,6 +144,34 @@
 	</div>
 </div>
 
+<!-- Carousel -->
+<div class="container mt-4">
+	<div id="carouselExample" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="<?=base_url();?>assets/user/images/Slider/carousel_1.jpg"
+					alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="<?=base_url();?>assets/user/images/Slider/carousel_2.jpg"
+					alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="<?=base_url();?>assets/user/images/Slider/carousel_4.jpg"
+					alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</div>
+
 <!-- Benefit -->
 
 <div class="benefit">
